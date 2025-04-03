6a0dbab9a1c0c6b69c2267956ca843e2b72f0bc1
Jorge5278 <A00837558@tec.mx>
2025-04-02 18:02:00 -0600

    t

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import "@ui5/webcomponents/dist/Assets.js"; // Ajusta la ruta según tu configuración y paquete
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
